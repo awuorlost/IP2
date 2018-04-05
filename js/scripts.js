1081dcaef9d56b780e6e71256060a8942f575f42
Awuor <awuorattyang@gmail.com>
2018-04-05 05:15:50 -0700

    delete js functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,1 @@
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
 
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
-function myFunction(x) {
-    x.classList.toggle("change");
-} 
